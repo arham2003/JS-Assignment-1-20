de474df773f499f08b90e49458c5f2ff6e5cd5de
arham2003 <arhamkx@gmail.com>
2020-06-15 22:53:58 +0500

    first JS Assignment

---
+++ app.js
@@ -0,0 +1,157 @@
+//ASSIGNMENT NO.1
+
+//chapter1
+
+// // 1
+// alert("Welcome to our Website!")
+// // 2
+// alert("Error! Please enter a valid password.")
+// // 3
+// alert("Welcome to JS Land..." + "\n" + "Happy Coding!")
+// // 4 
+//  alert("Welcome To JS Land!");
+//  alert("Happy Coding!" + "\n" + "" + "Prevent this page from creating additional dialogs.");
+// //5 in console
+
+
+// //chapter2
+
+// // 1
+// var username;
+
+// // 2 
+// var myName = "Arham";
+
+// // 3
+
+// var message;
+// message = "Hello World";
+// alert(message);
+
+// //4
+// var StudentName = prompt("Please Enter your Name", "Johne Doe");
+// var StudentAge = prompt("Please Enter Your Age" , "15 Years Old");
+// var StudentStatement = prompt("Please enter your statement" , "Certified Mobile Application development");
+
+// // var myData = [StudentName,StudentAge,StudentStatement];
+// alert(StudentName);
+// alert(StudentAge);
+// alert(StudentStatement);
+
+// //5
+// var pizza = ("PIZZA" + "\n" + "PIZZ" + "\n" + "PIZ" + "\n" + "PI" + "\n" + "P");
+// alert(pizza);
+
+// //6
+// var email;
+// email = ("My" + " " + "email" + " " + "address" + " " + "is" + " " + "example@example.com");
+// alert(email);
+
+// //7
+// var book = ("A smarter way to learn JavaScript");
+// alert("I am trying to learn from the book " + book);
+
+// //8
+// document.write("Yah! I can write HTML content through Javascript");
+
+// //9
+// var symbol = ("▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬");
+// alert(symbol);
+
+
+//Chapter 3
+
+//1
+// var Age = ("15 Years Old");
+// alert("I am" + " " + Age);
+
+//2
+
+
+//3
+// var birthYear = "2003"
+// document.write("My Birth Year is " + birthYear + "<br>")
+// document.write("Data type of my declared variable is number");
+
+//4
+
+// var Name = prompt("Enter your Name:" ,"Johne doe");
+// var Quantity = prompt("How many product you want to order?" ,"5");
+// var Product = prompt("Enter Your Product Title:", "T-shirt(s)");
+
+// document.write(Name + " " +  "Ordered " + Quantity + " " + Product + " " +  "on XYZ Clothing Store");
+
+
+//Chapter 4
+
+//1
+
+// var variable1 , variable2 , variable3;
+
+// //2
+
+// var #joota;
+// var var;
+// var userAlert;
+// var 2fight;
+// var ali hamza;
+
+
+// var myData;
+// var bHaiMera;
+// var Live_on;
+// var $paisa;
+// var khan441;
+
+//3
+
+// document.write("<br>");
+// document.write("Variable names can only contain , numbers, $ and _. For example : $my_1stVariable");
+// document.write("<br>");
+// document.write("Variables must begin with a letter, $ or _. For example : $name,_name or name");
+// document.write("<br>");
+// document.write("Variable names are case sensitive");
+// document.write("<br>");
+// document.write("Variable names should not be JS keywords");
+
+
+//Chapter 5
+
+//1
+
+// var num1 = 5;
+// var num2 = 10;
+// var sum = num1 + num2;
+
+// document.write("Sum of " + num1 + " " + "and " + num2 + " " + "is " + sum);
+
+//2
+
+var num1 = 5;
+var num2 = 10;
+var sum = num1 - num2;
+
+document.write("Sum of " + num1 + " " + "subtracted from " + num2 + " " + "is " + sum);
+document.write("<br>");
+
+var num1 = 5;
+var num2 = 10;
+var sum = num1 * num2;
+
+document.write("Sum of " + num1 + " " + "multiplied by " + num2 + " " + "is " + sum);
+document.write("<br>");
+
+var num1 = 5;
+var num2 = 10;
+var sum = num1 / num2;
+
+document.write("Sum of " + num1 + " " + "divided by " + num2 + " " + "is " + sum);
+document.write("<br>");
+
+var num1 = 5;
+var num2 = 10;
+var sum = num1 % num2;
+
+document.write("Sum of " + num1 + " " + "modulo " + num2 + " " + "is " + sum);
+document.write("<br>");
+
